2691b68b74d480255b83699257acd3f3b87904d9
morshul <morshu0@proton.me>
2023-04-22 09:22:20 -0300

    fix(legendary): register keymaps correctly
    Fixes an issue where keymaps were not registering correctly in the editor. The problem was caused by the `vim.tbl_extend` function and has been replaced by a function that merges the keymap tables.

--- lua/modules/packages/tooling/legendary-nvim.lua
+++ lua/modules/packages/tooling/legendary-nvim.lua
@@ -7,11 +7,20 @@ manifest.config = function()
   local plugin = require('legendary')
 
   plugin.setup({
-    keymaps = vim.tbl_extend(
-      'force',
-      require('legendary.keymaps.general'),
-      require('legendary.keymaps.file-explorer')
-    ),
+    keymaps = (function()
+      local keymaps = {}
+      local sections = { 'general', 'file-explorer' }
+
+      for _, section in pairs(sections) do
+        local mappings = require('legendary.keymaps.' .. section)
+
+        for _, keybind in pairs(mappings) do
+          table.insert(keymaps, keybind)
+        end
+      end
+
+      return keymaps
+    end)(),
   })
 end
 
